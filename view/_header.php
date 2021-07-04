ad997f0c9b012ce38860c7901acdb688f957271e
MarinHanic0 <marin.hanic1@gmail.com>
2021-07-04 17:01:19 +0200

    Napravljen header i implementirana login funkcionalnost buttona

--- view/_header.php
+++ view/_header.php
@@ -4,10 +4,96 @@
     <meta charset="utf8">
     <title><?php if (isset($title)) echo $title; else echo "ebuy"; ?></title>
     <link rel="stylesheet" href="<?php echo __SITE_URL; ?>/static/bootstrap/css/bootstrap.css">
-    <script type="text/javascript" src="<?php echo __SITE_URL; ?>/static/jquery-min.js"></script></head>
+    <link rel="stylesheet" href="<?php echo __SITE_URL; ?>/static/header.css">
+    <script type="text/javascript" src="<?php echo __SITE_URL; ?>/static/jquery-min.js"></script>    
+    </head>
 <body>
 <br>
-<h1>Welcome, <?php if (isset($_SESSION["user"])) echo $_SESSION["user"] -> getUsername(); else echo " please login or register!"; ?></h1>
+<h1>HOTEL BOOKING</h1>
+<h2><?php if (isset($_SESSION["user"])) {
+    echo "Pozdrav ". $_SESSION["user"] -> getUsername(); ?>
+        <form action="<?php echo __SITE_URL; ?>/login/processLogout">
+            <input type="submit" value="Logout" />
+        </form>
+    <?php
+    }
+    else{?>
+        <button id="loginbtn">Log in</button>
+
+            <div id="login" class="modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <?php if (isset($error) && isset($errorMessage) && $error) echo '<p class="alert alert-danger">' . $errorMessage . "</p>"; ?>
+
+                    <form method="post" action="<?php echo __SITE_URL . '/login/processLoginForm' ?>">
+                        <div class="form-group">
+                            <label for="username">Username:</label>
+                            <input class="form-control" id="username" name="username" type="text">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input class="form-control" id="password" name="password" type="password">
+                        </div>
+                        <br>
+                        <div class="float-end">
+                            <input class="btn btn-primary" type="submit" name="login" value="Login"/>
+                        </div>
+                        <br>
+                    </form>
+                </div>
+            </div>
+
+        <button id="joinbtn">Join us</button>
+
+            <div id="join" class="modal">
+                <div class= "modal-content">
+                    <span class="close">&times</span>
+                    <p>Join form</p>
+                </div>
+            </div>
+</h2>
+<?php } ?>
+
+<script>
+
+var loginModal = document.getElementById("login");
+
+var loginBtn = document.getElementById("loginbtn");
+
+var joinModal = document.getElementById("join");
+
+var joinBtn = document.getElementById("joinbtn");
+
+var loginSpan = document.getElementsByClassName("close")[0];
+var joinSpan = document.getElementsByClassName("close")[1];
+
+
+loginBtn.onclick = function() {
+    loginModal.style.display = "block";
+}
+
+joinBtn.onclick = function() {
+    joinModal.style.display = "block";
+}
+
+loginSpan.onclick = function() {
+    loginModal.style.display = "none";
+}
+
+joinSpan.onclick = function() {
+    joinModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+    if (event.target == joinModal) {
+        joinModal.style.display = "none";
+    }
+}
+</script>
 
 <?php
 if (isset($_SESSION["user"])) require "_navBar.php";
--- view/_navBar.php
+++ view/_navBar.php
@@ -4,6 +4,5 @@
         <li><a href="<?php echo __SITE_URL; ?>/hotels/ShoppingHistory">Visited hotels (shopping hitory)</a></li>
         <li><a href="<?php echo __SITE_URL; ?>/search">Search htoels</a></li>
         <li <?php if (!$_SESSION["user"]->getIs_admin()) echo "hidden"?> ><a href="<?php echo __SITE_URL; ?>/hotels/newHotel">Add new information about hotel</a></li>
-        <li><a href="<?php echo __SITE_URL; ?>/login/processLogout">Logout</a></li>
     </ul>
 </nav>
